b4152ba1adfde9a2b6c0bfcd0104114e8ba22444
manikandanxenon <manikandan@wearexenon.com>
2015-07-24 21:54:30 +0530

    decode obd

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var hexString = function(dec, length) {
 	return (Array(length + 1).join('0') + str).substring(str.length);
 };
 
+var parseHex = function(string) {
+	return parseInt(string, 16);
+};
+
 obd.isValidLoad = function(load) {
 	return ((load > 0) && (load < 100));
 };
@@ -58,7 +62,7 @@ var convertLoad = function(load) {
 };
 
 var convertRPM = function(rpm) {
-	return '4104' + hexString(rpm, 4);
+	return '4104' + hexString(rpm * 4, 4);
 };
 
 var convertSpeed = function(speed) {
@@ -66,7 +70,23 @@ var convertSpeed = function(speed) {
 };
 
 var convertMAF = function(maf) {
-	return '4110' + hexString(maf, 4);
+	return '4110' + hexString(Math.round(maf * 100), 4);
+};
+
+var decodeLoad = function(string) {
+	return parseHex(string.substring(0, 2)) * 100 / 255;
+};
+
+var decodeRPM = function(string) {
+	return parseHex(string.substring(0, 4)) / 4;
+};
+
+var decodeSpeed = function(string) {
+	return parseHex(string.substring(0, 2));
+};
+
+var decodeMAF = function(string) {
+	return parseHex(string.substring(0, 4)) / 100;
 };
 
 obd.encodeLoad = function(load) {
@@ -116,6 +136,59 @@ obd.encodeOBD = function(obdObject) {
 		encodedObd.push(convertMAF(obdObject.maf));
 	}
 	return encodedObd;
+};
+
+obd.decodeOBD = function(obd) {
+	var decodedObd = null;
+	var obdData;
+	if (typeof obd === 'string') {
+		obdData = obd.substring(4, obd.length);
+		switch (obd.substring(0, 4)) {
+			case '410C':
+				decodedObd = decodeLoad(obdData);
+				break;
+			case '4104':
+				decodedObd = decodeRPM(obdData);
+				break;
+			case '410D':
+				decodedObd = decodeSpeed(obdData);
+				break;
+			case '4110':
+				decodedObd = decodeMAF(obdData);
+				break;
+			default :
+				decodedObd = null;
+				break;
+		}
+	}else if (obd instanceof Array) {
+		decodedObd = {};
+		if (obd.length <= 0) {
+			return decodedObd;
+		} else {
+			for (var i = 0; i < obd.length; i += 1) {
+				obdData = obd[i].substring(4, obd[i].length);
+				switch (obd[i].substring(0, 4)) {
+					case '410C':
+						decodedObd.load = decodeLoad(obdData);
+						break;
+					case '4104':
+						decodedObd.rpm = decodeRPM(obdData);
+						break;
+					case '410D':
+						decodedObd.speed = decodeSpeed(obdData);
+						break;
+					case '4110':
+						decodedObd.maf = decodeMAF(obdData);
+						break;
+					default :
+						break;
+				}
+			}
+		}
+	}else {
+		decodedObd = {};
+	}
+	return decodedObd;
 };
 
 module.exports = obd;
--- test/index.spec.js
+++ test/index.spec.js
@@ -99,16 +99,16 @@ describe('obd', function() {
 	});
 	describe('obd.encodeRPM', function() {
 		it('should return encoded RPM for a single digit RPM', function() {
-			expect(obd.encodeRPM(15)).toEqual('4104000F');
+			expect(obd.encodeRPM(3.75)).toEqual('4104000F');
 		});
 		it('should return encoded RPM for a two digit RPM', function() {
-			expect(obd.encodeRPM(255)).toEqual('410400FF');
+			expect(obd.encodeRPM(63.75)).toEqual('410400FF');
 		});
 		it('should return encoded RPM for a three digit RPM', function() {
-			expect(obd.encodeRPM(256)).toEqual('41040100');
+			expect(obd.encodeRPM(64)).toEqual('41040100');
 		});
 		it('should return encoded RPM for a four digit RPM', function() {
-			expect(obd.encodeRPM(16000)).toEqual('41043E80');
+			expect(obd.encodeRPM(4000)).toEqual('41043E80');
 		});
 		it('should return empty string for invalid RPM', function() {
 			expect(obd.encodeRPM(-101)).toEqual('');
@@ -124,16 +124,16 @@ describe('obd', function() {
 	});
 	describe('obd.encodeMAF', function() {
 		it('should return encoded Load for a single digit MAF', function() {
-			expect(obd.encodeMAF(15)).toEqual('4110000F');
+			expect(obd.encodeMAF(0.15)).toEqual('4110000F');
 		});
 		it('should return encoded Load for a two digit MAF', function() {
-			expect(obd.encodeMAF(255)).toEqual('411000FF');
+			expect(obd.encodeMAF(2.55)).toEqual('411000FF');
 		});
 		it('should return encoded Load for a three digit MAF', function() {
-			expect(obd.encodeMAF(256)).toEqual('41100100');
+			expect(obd.encodeMAF(2.56)).toEqual('41100100');
 		});
 		it('should return empty string for four digit invalid MAF', function() {
-			expect(obd.encodeMAF(16000)).toEqual('');
+			expect(obd.encodeMAF(655.35)).toEqual('');
 		});
 		it('should return empty string for invalid MAF', function() {
 			expect(obd.encodeMAF(-101)).toEqual('');
@@ -147,7 +147,7 @@ describe('obd', function() {
 				speed : 128,
 				maf : 500
 			};
-			var obdArray = ['410C99', '41040800', '410D80', '411001F4'];
+			var obdArray = ['410C99', '41042000', '410D80', '4110C350'];
 			expect(obd.encodeOBD(obdObject)).toEqual(obdArray);
 		});
 		it('should return encoded OBD array for Valid and Invalid parameters', function() {
@@ -166,6 +166,55 @@ describe('obd', function() {
 			};
 			var obdArray = [];
 			expect(obd.encodeOBD(obdObject)).toEqual(obdArray);
+		});
+	});
+	describe('obd.decodeOBD', function() {
+		it('should decode obd load', function() {
+			expect(obd.decodeOBD('410C99')).toEqual(60);
+		});
+		it('should decode obd RPM', function() {
+			expect(obd.decodeOBD('41040800')).toEqual(512);
+		});
+		it('should decode obd speed', function() {
+			expect(obd.decodeOBD('410D80')).toEqual(128);
+		});
+		it('should decode obd MAF', function() {
+			expect(obd.decodeOBD('411001F4')).toEqual(5);
+		});
+		it('should decode invalid obd', function() {
+			expect(obd.decodeOBD('411101F4')).toEqual(null);
+		});
+		it('should return decoded OBD object for valid OBD array', function() {
+			var obdArray = ['410C99', '41040800', '410D80', '411001F4'];
+			var obdObject = {
+				load : 60,
+				rpm : 512,
+				speed : 128,
+				maf : 5
+			};
+			expect(obd.decodeOBD(obdArray)).toEqual(obdObject);
+		});
+		it('should return decoded OBD object for Valid and Invalid parameters', function() {
+			var obdArray = ['410C99', '41040800', '410D80', '411101F4'];
+			var obdObject = {
+				load : 60,
+				rpm : 512,
+				speed : 128
+			};
+			expect(obd.decodeOBD(obdArray)).toEqual(obdObject);
+		});
+		it('should return empty OBD Object for invalid OBD Array', function() {
+			var obdArray = ['411D80'];
+			var obdObject = {};
+			expect(obd.decodeOBD(obdArray)).toEqual(obdObject);
+		});
+		it('should return empty OBD Object for empty OBD Array', function() {
+			var obdArray = [];
+			var obdObject = {};
+			expect(obd.decodeOBD(obdArray)).toEqual(obdObject);
+		});
+		it('should decode invalid input', function() {
+			expect(obd.decodeOBD({})).toEqual({});
 		});
 	});
 });
